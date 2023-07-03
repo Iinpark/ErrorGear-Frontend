12fe776a0b337e6a60b56226322e5e1daf78be7c
Sultan <linpark11197@gmail.com>
2023-07-03 22:34:36 +0600

    initial

---
+++ src/components/FileInput.tsx
@@ -0,0 +1,40 @@
+import { InboxOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+import { message, Upload } from 'antd';
+import React from 'react';
+
+const { Dragger } = Upload;
+
+const FileInput = (props) => {
+  const { onFileUpload = () => {} } = props;
+
+  const uploadProps: UploadProps = {
+    name: 'file',
+    multiple: false,
+    action(file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        if (!event) return;
+
+        const contents = event.target.result;
+        onFileUpload(contents);
+      };
+      reader.readAsText(file);
+
+      return Promise.reject();
+    },
+  };
+
+  return (
+    <Dragger {...uploadProps}>
+      <p className='ant-upload-drag-icon'>
+        <InboxOutlined />
+      </p>
+      <p className='ant-upload-text'>
+        Click or drag file to this area to upload
+      </p>
+    </Dragger>
+  );
+};
+
+export default FileInput;
