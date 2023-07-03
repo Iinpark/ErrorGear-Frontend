dc3d47c82ba013e36cf54163deee6b1fca51dc9d
Sultan <linpark11197@gmail.com>
2023-07-03 23:20:57 +0600

    верстка

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
 import FileInput from './components/FileInput';
+import { Layout, Col, Row } from 'antd';
 
 function App() {
   const [parsedFile, setParsedFile] = useState('');
+  const span = 12;
   return (
     <>
-      <FileInput onFileUpload={setParsedFile} />
-      <pre>{parsedFile}</pre>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Layout.Content style={{ paddingTop: '8px' }}>
+          <Row>
+            <Col span={span} offset={span / 2}>
+              <FileInput onFileUpload={setParsedFile} />
+              <pre>{parsedFile}</pre>
+            </Col>
+          </Row>
+        </Layout.Content>
+
+        <Layout.Footer style={{ textAlign: 'center' }}>
+          Made on Earth by Humans
+        </Layout.Footer>
+      </Layout>
     </>
   );
 }
